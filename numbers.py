97ab828d3cf86ebca69b87cf343da439fd6f81bd
Kjetils0n <122093682+Kjetils0n@users.noreply.github.com>
2025-01-23 22:04:44 +0100

    Add files via upload

---
+++ numbers.py
@@ -0,0 +1,25 @@
+variable = 5 #int
+print(variable)
+
+number = 2
+print(number)
+
+number = 5
+print(number)
+
+x, y = 12, 42.2
+print(f"x: {x}")
+print("y:", y)
+
+a = 2
+b = 2
+a = a + b
+b = a + b
+print (a)
+print (b)
+
+dolphins_sleep_with_one_eye_open = True
+print (dolphins_sleep_with_one_eye_open)
+
+
+
